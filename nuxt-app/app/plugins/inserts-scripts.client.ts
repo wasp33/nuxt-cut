42dd489bc3d3189c70780d417c809bcfa012ccb5
Joseph Liu <drowsy.liu@gmail.com>
2025-09-18 13:33:11 +0800

    update bw canvas

--- nuxt-app/app/plugins/inserts-scripts.client.ts
+++ nuxt-app/app/plugins/inserts-scripts.client.ts
@@ -1,13 +1,13 @@
 export default defineNuxtPlugin(() => {
   // 插到 <head>
-  const headScript = document.createElement('script')
-  headScript.id = '_bownow_ts'
-  headScript.textContent = `var _bownow_ts = document.createElement("script");
-      _bownow_ts.charset = "utf-8";
-      _bownow_ts.src =
-        "https://contents.bownow.jp/js/UTC_373f6c2fd8a84cc0e8f9/trace.js";
-      document.getElementsByTagName("head")[0].appendChild(_bownow_ts);`
-  document.head.appendChild(headScript)
+  // const headScript = document.createElement('script')
+  // headScript.id = '_bownow_ts'
+  // headScript.textContent = `var _bownow_ts = document.createElement("script");
+  //     _bownow_ts.charset = "utf-8";
+  //     _bownow_ts.src =
+  //       "https://contents.bownow.jp/js/UTC_373f6c2fd8a84cc0e8f9/trace.js";
+  //     document.getElementsByTagName("head")[0].appendChild(_bownow_ts);`
+  // document.head.appendChild(headScript)
 
   // 插到 <body>
   // const bodyScript = document.createElement('script')
