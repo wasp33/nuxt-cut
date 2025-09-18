acb921b61b55d222c6a4b26b5f3d58904b49c1ac
Joseph Liu <drowsy.liu@gmail.com>
2025-09-18 12:19:07 +0800

    use plugin

--- nuxt-app/pages/about.vue
+++ nuxt-app/pages/about.vue
@@ -29,14 +29,14 @@
 <script setup>
 if (process.client) {
   // 1) 插到 <head>
-  const sHead = document.createElement('script')
-  sHead.id = '_bownow_ts'
-  sHead.textContent = `var _bownow_ts = document.createElement("script");
-      _bownow_ts.charset = "utf-8";
-      _bownow_ts.src =
-        "https://contents.bownow.jp/js/UTC_373f6c2fd8a84cc0e8f9/trace.js";
-      document.getElementsByTagName("head")[0].appendChild(_bownow_ts);`
-  document.head.appendChild(sHead)
+  // const sHead = document.createElement('script')
+  // sHead.id = '_bownow_ts'
+  // sHead.textContent = `var _bownow_ts = document.createElement("script");
+  //     _bownow_ts.charset = "utf-8";
+  //     _bownow_ts.src =
+  //       "https://contents.bownow.jp/js/UTC_373f6c2fd8a84cc0e8f9/trace.js";
+  //     document.getElementsByTagName("head")[0].appendChild(_bownow_ts);`
+  // document.head.appendChild(sHead)
 
   // 2) 插到 <body> 尾端
   const sBody = document.createElement('script')
